fb8e1cf32760fba149d1762764082a51cd144625
Peter Chang <pech6593@colorado.edu>
2022-01-27 12:59:51 -0700

    averaged a little better

--- 01-26-2022_Data_HCO_CO_simultaneous.py
+++ 01-26-2022_Data_HCO_CO_simultaneous.py
@@ -10,58 +10,6 @@ import scipy.constants as sc
 
 # clipboard_and_style_sheet.style_sheet()
 
-
-# %%
-# path = r'G:\.shortcut-targets-by-id\1cPwz25CLF5JBH9c_yF0vSr5p3Bl_1-nM\MIR GHz DSC\220126/'
-# data = np.fromfile(path + "CO_filter_60972x65484.bin", '<h')
-#
-# # %%
-# ppifg = 60972 // 2
-# Nifgs = 65484
-#
-# # %%
-# data = data[ppifg // 2:-ppifg // 2]
-# data.resize(Nifgs - 1, ppifg)
-#
-#
-# # %%
-# def computeshifts():
-#     zoom = data[:, ppifg // 2 - 200:ppifg // 2 + 200]
-#     ftzoom = np.fft.ifftshift(mkl_fft.fft(np.fft.ifftshift(zoom, axes=1), axis=1), axes=1)
-#
-#     # getting rid of f0 for H2CO (comment out these two lines for the CO one)
-#     # ftzoom[:, 300:] = 0.0
-#     # ftzoom[:, :100] = 0.0
-#
-#     corrW = ftzoom * ftzoom[0].conj()
-#     corrW = np.pad(corrW, ([0, 0], [2 ** 10, 2 ** 10]), constant_values=0.0)
-#     corr = np.fft.ifftshift(mkl_fft.ifft(np.fft.ifftshift(corrW, axes=1), axis=1), axes=1)
-#     inds = np.argmax(corr, axis=1) - len(corr[0]) // 2
-#     shifts = inds * len(zoom[0]) / len(corr[0])
-#     return shifts
-#
-#
-# shifts = computeshifts()
-#
-# # %%
-#
-# # way too large to be handled all at one time in memory (32 GB), so use a for loop instead
-# freq = np.fft.fftfreq(len(data[0]))
-# for n, i in enumerate(data):
-#     data[n] = np.fft.fftshift(mkl_fft.ifft(mkl_fft.fft(np.fft.ifftshift(i))
-#                                            * np.exp(1j * 2 * np.pi * freq * shifts[n]))).real
-#     if n % 100 == 0:
-#         print('{n}/{N}'.format(n=n, N=Nifgs))
-#
-# # %%
-# data.tofile(path + f"CO_filter_{Nifgs}x{ppifg}_phase_corrected.bin")
-#
-# # %%
-# avg = np.mean(data, axis=0)
-# ftavg = np.fft.fftshift(mkl_fft.fft(np.fft.ifftshift(avg)))
-
-# %% after phase correction
-
 def fft(x, axis=None):
     """
     calculates the 1D fft of the numpy array x
@@ -87,6 +35,61 @@ def ifft(x, axis=None):
 
 
 # %%
+# path = r'G:\.shortcut-targets-by-id\1cPwz25CLF5JBH9c_yF0vSr5p3Bl_1-nM\MIR GHz DSC\220126/'
+# data = np.fromfile(path + "H2CO_filter_60972x65484.bin", '<h')
+#
+# # %%
+# ppifg = 60972 // 2
+# Nifgs = 65484
+#
+# # %%
+# plotsec = lambda vec: plt.plot(vec[ppifg // 2 - 100: ppifg // 2 + 100])
+#
+# # %%
+# data = data[ppifg // 2:-ppifg // 2]
+# data.resize(Nifgs - 1, ppifg)
+#
+#
+# # %%
+# def computeshifts():
+#     zoom = data[:, ppifg // 2 - 200:ppifg // 2 + 200]
+#     ftzoom = fft(zoom)
+#
+#     # getting rid of f0 for H2CO (comment out this block for the CO one)
+#     ftzoom[:, 300:] = 0.0
+#     ftzoom[:, :100] = 0.0
+#     zoom = ifft(ftzoom).real
+#     zoom = zoom[:, 50:-50]  # some ringing on the edge due to the brick wall filter
+#     ftzoom = fft(zoom)
+#
+#     corrW = ftzoom * ftzoom[0].conj()
+#     corrW = np.pad(corrW, ([0, 0], [2 ** 10, 2 ** 10]), constant_values=0.0)
+#     corr = ifft(corrW)
+#     inds = np.argmax(corr, axis=1) - len(corr[0]) // 2
+#     shifts = inds * len(zoom[0]) / len(corr[0])
+#     return shifts
+#
+#
+# shifts = computeshifts()
+#
+# # %%
+#
+# # way too large to be handled all at one time in memory (32 GB), so use a for loop instead
+# freq = np.fft.fftfreq(len(data[0]))
+# for n, i in enumerate(data):
+#     data[n] = np.fft.fftshift(mkl_fft.ifft(mkl_fft.fft(np.fft.ifftshift(i))
+#                                            * np.exp(1j * 2 * np.pi * freq * shifts[n]))).real
+#     if n % 100 == 0:
+#         print('{n}/{N}'.format(n=n, N=Nifgs))
+#
+# # %%
+# data.tofile(path + f"H2CO_filter_{Nifgs}x{ppifg}_phase_corrected.bin")
+#
+# # %%
+# avg = np.mean(data, axis=0)
+# ftavg = np.fft.fftshift(mkl_fft.fft(np.fft.ifftshift(avg)))
+
+# %% after phase correction
 path = r'G:\.shortcut-targets-by-id\1cPwz25CLF5JBH9c_yF0vSr5p3Bl_1-nM\MIR GHz DSC\220126/'
 dataH2CO = np.fromfile(path + "H2CO_filter_65484x30486_phase_corrected.bin", '<h')
 dataH2CO.resize(65484, 30486)
