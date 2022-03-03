306c5204572d59fe82913b3f059d36a9b29a093f
Peter Chang <pech6593@colorado.edu>
2022-03-03 01:01:14 -0700

    update

--- 03-01-2022_Plotting.py
+++ 03-01-2022_Plotting.py
@@ -4,41 +4,15 @@ import clipboard_and_style_sheet
 import mkl_fft
 
 # %%
-dataCO = np.fromfile("Data/03-01-2022/CO_filter_phasecorr_9995x199728.bin", 'h')
-dataH2CO = np.fromfile("Data/03-01-2022/H2CO_filter_phasecorr_9995x199728.bin", 'h')
-dataH2CO.resize(9995, 199728)
-dataCO.resize(9995, 199728)
+data = np.fromfile("Data/03-01-2022/H2CO_filter_phasecorr_9995x199728.bin", 'h')
+data.resize(9995, 199728)
+data = data.astype(np.float64)
 
 # %%
-avgH2CO = np.mean(dataH2CO, axis=0)
-ftH2CO = np.fft.fftshift(mkl_fft.fft(np.fft.ifftshift(avgH2CO)))
-
-avgCO = np.mean(dataCO, axis=0)
-ftCO = np.fft.fftshift(mkl_fft.fft(np.fft.ifftshift(avgCO)))
-
-# %% filter f0 for CO
-ftCO[38000:96000] = 0.0
-ftCO[106000:162000] = 0.0
-avgCO = np.fft.fftshift(mkl_fft.ifft(np.fft.ifftshift(ftCO))).real
-
-# %% filter f0 for H2CO
-ftH2CO[101000:170000] = 0
-ftH2CO[25000:98000] = 0
-avgH2CO = np.fft.fftshift(mkl_fft.ifft(np.fft.ifftshift(ftH2CO))).real
 
 # %%
-plt.figure()
-plt.title("average H2CO")
-plt.plot(avgH2CO)
+for n, dat in enumerate(data):
+    data[n] = (data[n] / (n + 1)) + (data[n - 1] * (n / (n + 1)))
 
-plt.figure()
-plt.title("average H2CO")
-plt.plot(ftH2CO.__abs__())
-
-plt.figure()
-plt.title("average CO")
-plt.plot(avgCO)
-
-plt.figure()
-plt.title("average CO")
-plt.plot(ftCO.__abs__())
+    if n % 100 == 0:
+        print(n)
