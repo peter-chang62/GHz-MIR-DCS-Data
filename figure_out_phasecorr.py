3c79d2350dac66cba85d06e2d6c9b6e861399dc6
Peter <pech6593@colorado.edu>
2022-04-26 16:05:37 -0600

    latest update

---
+++ figure_out_phasecorr.py
@@ -0,0 +1,184 @@
+import numpy as np
+import mkl_fft
+import PyQt5.QtWidgets as qt
+from scipy.signal import windows as wd
+import os
+import matplotlib.pyplot as plt
+
+
+def fft(x, axis=None):
+    """
+    calculates the 1D fft of the numpy array x
+    if x is not 1D you need to specify the axis
+    """
+
+    if axis is None:
+        return np.fft.fftshift(mkl_fft.fft(np.fft.ifftshift(x)))
+    else:
+        return np.fft.fftshift(mkl_fft.fft(np.fft.ifftshift(x, axes=axis), axis=axis), axes=axis)
+
+
+def ifft(x, axis=None):
+    """
+    calculates the 1D ifft of the numpy array x
+    if x is not 1D you need to specify the axis
+    """
+
+    if axis is None:
+        return np.fft.fftshift(mkl_fft.ifft(np.fft.ifftshift(x)))
+    else:
+        return np.fft.fftshift(mkl_fft.ifft(np.fft.ifftshift(x, axes=axis), axis=axis), axes=axis)
+
+
+def normalize(vec):
+    return vec / np.max(abs(vec))
+
+
+def rad_to_deg(rad):
+    return rad * 180 / np.pi
+
+
+def deg_to_rad(deg):
+    return deg * np.pi / 180
+
+
+def get_data(path, N_toanalyze):
+    """
+    :param path: path to data folder containing all the segment files
+    :param N_toanalyze: which segment file to analyze (starting from 1)
+    :return: data as a 1D array
+    """
+    names = [i.name for i in os.scandir(path)]
+    key = lambda f: int(f.split('LoopCount_')[1].split('_Datetime')[0])
+    names = sorted(names, key=key)
+
+    # %% load a single data file and throw out the time stamp
+    data = np.fromfile(path + names[N_toanalyze], '<h')
+    data = data[:-64]
+
+    return data
+
+
+def get_ind_total_to_throw(data, ppifg):
+    """
+    :param data: data as a 1D array
+    :param ppifg: points per interferogram (int)
+
+    :return:
+    the index that marks the incident shock,
+    the index that marks the reflected shock
+    """
+    center = ppifg // 2
+
+    # %% skip to the max of the first interferogram, and then ppifg // 2 after that
+    start = data[:ppifg]
+    ind_THREW_OUT = np.argmax(start)
+    data = data[ind_THREW_OUT:]
+    N = len(data) // ppifg
+    data = data[:N * ppifg]
+    data = data[ppifg // 2: - ppifg // 2]
+    N = len(data) // ppifg
+    data = data.reshape(N, ppifg)
+
+    # %% how do you find the start of the transient?
+    bckgnd = np.copy(data)
+    # remove all the interferograms
+    bckgnd[:, center - 50:center + 50] = 0.0
+    # the first maximum of the baseline gives the incident wave
+    ind_incident = np.argmax(bckgnd.flatten()[:int(3e6)])
+    # clear the incident wave, and look for the reflected one
+    # don't let it look too far, in case there are subsequent shocks from reflection off the far end wall
+    skip = ind_incident + int(1e4)
+    ind_reflected = np.argmax(bckgnd.flatten()[skip:skip + int(2e5)]) + skip
+
+    # %% skip to the max of the first interferogram, then ppifg // 2 after that, and then add on ind_reflected
+    ind_incident += ind_THREW_OUT + ppifg // 2
+    ind_reflected += ind_THREW_OUT + ppifg // 2
+
+    return ind_incident, ind_reflected
+
+
+def adjust_data_and_reshape(data, ppifg):
+    """
+    :param data:
+    :param ppifg:
+
+    :return: data truncated (both start and end) to an integer number of interferograms and reshaped.
+    because it might be important to know, I also return the number of points I truncated
+    from the start of the data
+    """
+
+    # skip to the max of the first interferogram, and then NEGATIVE or POSITIVE ppifg // 2 after that
+    start = data[:ppifg]
+    ind = np.argmax(abs(start))
+    if ind > ppifg // 2:
+        ind -= ppifg // 2
+    elif ind < ppifg // 2:
+        ind += ppifg // 2
+
+    data = data[ind:]
+    N = len(data) // ppifg
+    data = data[:N * ppifg]
+    N = len(data) // ppifg
+    data = data.reshape(N, ppifg)
+    return data, ind
+
+
+def Phase_Correct(data, ppifg, N_zoom=50, plot=True):
+    """
+    :param data: data as a 2D array
+    :param ppifg: points per interferogram
+    :param N_zoom: number of data points in the zoomed in window from calculating the phase correction
+    :param plot: plot the phase correction (diagnostics)
+
+    :return: phase corrected data as a 2D array
+    """
+    center = ppifg // 2
+    # %% zoomed in data
+    zoom = data[:, center - 200: center + 201].astype(float)
+    zoom = (zoom.T - np.mean(zoom, 1)).T
+
+    # %% appodize to remove f0, use a window of size 50
+    window = wd.blackman(N_zoom)
+    left = (len(zoom[0]) - N_zoom) // 2
+    right = len(zoom[0]) - N_zoom - left
+    window = np.pad(window, (left, right), constant_values=0)
+    zoom_appod = zoom * window
+
+    # %% calculate the shifts
+    fft_zoom = fft(zoom_appod, 1)
+    ref = fft_zoom[0]
+    fft_zoom *= np.conj(ref)
+    fft_zoom = np.pad(fft_zoom, ([0, 0], [2 ** 10, 2 ** 10]), constant_values=0.0)
+    fft_zoom = ifft(fft_zoom, 1)
+    ind = np.argmax(fft_zoom, axis=1) - len(fft_zoom[0]) // 2
+    shift = ind * len(zoom[0]) / len(fft_zoom[0])
+
+    # %% shift correct data
+    ft = np.fft.fft(np.fft.ifftshift(data, axes=1), axis=1)
+    freq = np.fft.fftfreq(len(ft[0]))
+    phase = np.zeros(data.shape).astype(np.complex128)
+    phase[:] = 1j * 2 * np.pi * freq
+    phase = (phase.T * shift).T
+    phase = np.exp(phase)
+    ft *= phase
+    ft = np.fft.fftshift(np.fft.ifft(ft, axis=1), axes=1)
+    phase_corr = ft.real
+
+    if plot:
+        # a view of the appodization method for removal of f0
+        fig, ax = plt.subplots(1, 2, figsize=np.array([11.9, 4.8]))
+        ax[0].plot(normalize(zoom[0]))
+        ax[0].plot(window)
+        ax[1].plot(normalize(zoom[0] * window))
+
+        # check the phase correction
+        fig, ax = plt.subplots(1, 2, figsize=np.array([11.9, 4.8]))
+        [ax[1].plot(i[center - 100:center + 100]) for i in phase_corr[:50]]
+        [ax[1].plot(i[center - 100:center + 100]) for i in phase_corr[-50:]]
+        [ax[0].plot(i[center - 100:center + 100]) for i in data[:50]]
+        [ax[0].plot(i[center - 100:center + 100]) for i in data[-50:]]
+        ax[0].set_title("un corrected")
+        ax[1].set_title("corrected")
+
+    return phase_corr
--- scratch.py
+++ scratch.py
@@ -1,232 +1,16 @@
 import numpy as np
-import mkl_fft
-import PyQt5.QtWidgets as qt
-from scipy.signal import windows as wd
-import os
 import matplotlib.pyplot as plt
+import clipboard_and_style_sheet
+import figure_out_phasecorr as pc
 
 
-def fft(x, axis=None):
-    """
-    calculates the 1D fft of the numpy array x
-    if x is not 1D you need to specify the axis
-    """
-
-    if axis is None:
-        return np.fft.fftshift(mkl_fft.fft(np.fft.ifftshift(x)))
-    else:
-        return np.fft.fftshift(mkl_fft.fft(np.fft.ifftshift(x, axes=axis), axis=axis), axes=axis)
+def rad_to_deg(rad):
+    return rad * 180 / np.pi
 
 
-def ifft(x, axis=None):
-    """
-    calculates the 1D ifft of the numpy array x
-    if x is not 1D you need to specify the axis
-    """
+def deg_to_rad(deg):
+    return deg * np.pi / 180
 
-    if axis is None:
-        return np.fft.fftshift(mkl_fft.ifft(np.fft.ifftshift(x)))
-    else:
-        return np.fft.fftshift(mkl_fft.ifft(np.fft.ifftshift(x, axes=axis), axis=axis), axes=axis)
-
-
-def normalize(vec):
-    return vec / np.max(abs(vec))
-
-
-def get_data(path, N_toanalyze):
-    names = [i.name for i in os.scandir(path)]
-    key = lambda f: int(f.split('LoopCount_')[1].split('_Datetime')[0])
-    names = sorted(names, key=key)
-
-    # %% load a single data file and throw out the time stamp
-    data = np.fromfile(path + names[N_toanalyze], '<h')
-    data = data[:-64]
-
-    return data
-
-
-def get_ind_total_to_throw(data, ppifg):
-    center = ppifg // 2
-
-    # %% skip to the max of the first interferogram, and then ppifg // 2 after that
-    start = data[:ppifg]
-    ind_THREW_OUT = np.argmax(start)
-    data = data[ind_THREW_OUT:]
-    N = len(data) // ppifg
-    data = data[:N * ppifg]
-    data = data[ppifg // 2: - ppifg // 2]
-    N = len(data) // ppifg
-    data = data.reshape(N, ppifg)
-
-    # %% how do you find the start of the transient?
-    hey = np.copy(data)
-    # remove all the interferograms
-    hey[:, center - 50:center + 50] = 0.0
-    # the first maximum of the baseline gives the incident wave
-    ind_incident = np.argmax(hey.flatten()[:int(3e6)])
-    # clear the incident wave, and look for the reflected one
-    # don't let it look too far, in case there are subsequent shocks from reflection off the far end wall
-    skip = ind_incident + int(1e4)
-    ind_reflected = np.argmax(hey.flatten()[skip:skip + int(2e5)]) + skip
-
-    # %% to reference the time location of the shock wave of H2CO based on CO:
-    # skip to the max of the first interferogram, then ppifg // 2 after that, and then add on ind_reflected
-    IND_TOTAL_TO_THROW = ind_THREW_OUT + ppifg // 2 + ind_reflected
-
-    return IND_TOTAL_TO_THROW, hey, ind_incident, ind_reflected
-
-
-def analyze(path, ppifg, N_toanalyze, plot=True, IND_TOTAL_TO_THROW=None, N_zoom=50, N_truncate=-1):
-    center = ppifg // 2
-
-    # %%
-    data = get_data(path, N_toanalyze)
-
-    if IND_TOTAL_TO_THROW is None:
-        plot_hey = True
-        IND_TOTAL_TO_THROW, hey, ind_incident, ind_reflected = get_ind_total_to_throw(data, ppifg)
-    else:
-        plot_hey = False
-
-    data = data[IND_TOTAL_TO_THROW:]
-
-    # %% skip to the max of the first interferogram, and then NEGATIVE or POSITIVE ppifg // 2 after that
-    start = data[:ppifg]
-    ind_THREW_OUT = np.argmax(abs(start))
-    if ind_THREW_OUT > ppifg // 2:
-        ind_THREW_OUT -= ppifg // 2
-    elif ind_THREW_OUT < ppifg // 2:
-        ind_THREW_OUT += ppifg // 2
-
-    data = data[ind_THREW_OUT:]
-    N = len(data) // ppifg
-    data = data[:N * ppifg]
-    N = len(data) // ppifg
-    data = data.reshape(N, ppifg)
-
-    if not N_truncate == -1:
-        data = data[:N_truncate]
-
-    # %% zoomed in data
-    zoom = data[:, center - 200: center + 201].astype(float)
-    zoom = (zoom.T - np.mean(zoom, 1)).T
-
-    # %% appodize to remove f0, use a window of size 50
-    window = wd.blackman(N_zoom)
-    left = (len(zoom[0]) - N_zoom) // 2
-    right = len(zoom[0]) - N_zoom - left
-    window = np.pad(window, (left, right), constant_values=0)
-    zoom_appod = zoom * window
-
-    # %% calculate the shifts
-    fft_zoom = fft(zoom_appod, 1)
-    ref = fft_zoom[0]
-    fft_zoom *= np.conj(ref)
-    fft_zoom = np.pad(fft_zoom, ([0, 0], [2 ** 10, 2 ** 10]), constant_values=0.0)
-    fft_zoom = ifft(fft_zoom, 1)
-    ind = np.argmax(fft_zoom, axis=1) - len(fft_zoom[0]) // 2
-    shift = ind * len(zoom[0]) / len(fft_zoom[0])
-
-    # %% shift correct data
-    ft = np.fft.fft(np.fft.ifftshift(data, axes=1), axis=1)
-    freq = np.fft.fftfreq(len(ft[0]))
-    phase = np.zeros(data.shape).astype(np.complex128)
-    phase[:] = 1j * 2 * np.pi * freq
-    phase = (phase.T * shift).T
-    phase = np.exp(phase)
-    ft *= phase
-    ft = np.fft.fftshift(np.fft.ifft(ft, axis=1), axes=1)
-    phase_corr = ft.real
-
-    # %% a lot of diagnostic plots
-    if plot:
-        # check the phase correction
-        fig, ax = plt.subplots(1, 2, figsize=np.array([11.9, 4.8]))
-        [ax[1].plot(i[center - 100:center + 100]) for i in phase_corr[:50]]
-        [ax[1].plot(i[center - 100:center + 100]) for i in phase_corr[-50:]]
-        [ax[0].plot(i[center - 100:center + 100]) for i in data[:50]]
-        [ax[0].plot(i[center - 100:center + 100]) for i in data[-50:]]
-        ax[0].set_title("un corrected")
-        ax[1].set_title("corrected")
-
-        # a view of the appodization method for removal of f0
-        fig, ax = plt.subplots(1, 2, figsize=np.array([11.9, 4.8]))
-        ax[0].plot(normalize(zoom[0]))
-        ax[0].plot(window)
-        ax[1].plot(normalize(zoom[0] * window))
-
-        # average time domain
-        plt.figure()
-        avg = np.mean(phase_corr, 0)
-        plt.plot(avg)
-
-        # average frequency domain
-        plt.figure()
-        plt.plot(fft(avg).__abs__())
-
-        if plot_hey:
-            # a view of where the program thought the reflected shock was
-            plt.figure()
-            plt.plot(hey.flatten())
-            plt.axvline(ind_incident, color='r')
-            plt.axvline(ind_reflected, color='r')
-            start = ind_reflected + ind_THREW_OUT
-            plt.plot(np.arange(start, start + len(data.flatten())), data.flatten())
-
-    return phase_corr, IND_TOTAL_TO_THROW
-
-
-def Phase_Correct(data, ppifg, N_zoom=50, plot=True):
-    center = ppifg // 2
-    # %% zoomed in data
-    zoom = data[:, center - 200: center + 201].astype(float)
-    zoom = (zoom.T - np.mean(zoom, 1)).T
-
-    # %% appodize to remove f0, use a window of size 50
-    window = wd.blackman(N_zoom)
-    left = (len(zoom[0]) - N_zoom) // 2
-    right = len(zoom[0]) - N_zoom - left
-    window = np.pad(window, (left, right), constant_values=0)
-    zoom_appod = zoom * window
-
-    # %% calculate the shifts
-    fft_zoom = fft(zoom_appod, 1)
-    ref = fft_zoom[0]
-    fft_zoom *= np.conj(ref)
-    fft_zoom = np.pad(fft_zoom, ([0, 0], [2 ** 10, 2 ** 10]), constant_values=0.0)
-    fft_zoom = ifft(fft_zoom, 1)
-    ind = np.argmax(fft_zoom, axis=1) - len(fft_zoom[0]) // 2
-    shift = ind * len(zoom[0]) / len(fft_zoom[0])
-
-    # %% shift correct data
-    ft = np.fft.fft(np.fft.ifftshift(data, axes=1), axis=1)
-    freq = np.fft.fftfreq(len(ft[0]))
-    phase = np.zeros(data.shape).astype(np.complex128)
-    phase[:] = 1j * 2 * np.pi * freq
-    phase = (phase.T * shift).T
-    phase = np.exp(phase)
-    ft *= phase
-    ft = np.fft.fftshift(np.fft.ifft(ft, axis=1), axes=1)
-    phase_corr = ft.real
-
-    if plot:
-        # a view of the appodization method for removal of f0
-        fig, ax = plt.subplots(1, 2, figsize=np.array([11.9, 4.8]))
-        ax[0].plot(normalize(zoom[0]))
-        ax[0].plot(window)
-        ax[1].plot(normalize(zoom[0] * window))
-
-        # check the phase correction
-        fig, ax = plt.subplots(1, 2, figsize=np.array([11.9, 4.8]))
-        [ax[1].plot(i[center - 100:center + 100]) for i in phase_corr[:50]]
-        [ax[1].plot(i[center - 100:center + 100]) for i in phase_corr[-50:]]
-        [ax[0].plot(i[center - 100:center + 100]) for i in data[:50]]
-        [ax[0].plot(i[center - 100:center + 100]) for i in data[-50:]]
-        ax[0].set_title("un corrected")
-        ax[1].set_title("corrected")
-
-    return phase_corr
 
 # %%
 path_co = r'D:\ShockTubeData\Data_04232022\Surf_18\card1/'
@@ -234,47 +18,102 @@ path_h2co = r'D:\ShockTubeData\Data_04232022\Surf_18\card2/'
 ppifg = 17511
 
 # %%
-N_Truncate = 100
+H2CO = np.zeros((248, ppifg))
+for n in range(248):
+    co = pc.get_data(path_co, n)
+    ind_i, ind_r = pc.get_ind_total_to_throw(co, ppifg)
 
-co, ind = analyze(path=path_co,
-                  ppifg=ppifg,
-                  N_toanalyze=15,
-                  plot=True,
-                  IND_TOTAL_TO_THROW=None,
-                  N_truncate=N_Truncate)
-h2co, _ = analyze(path=path_h2co,
-                  ppifg=ppifg,
-                  N_toanalyze=15,
-                  plot=True,
-                  IND_TOTAL_TO_THROW=ind,
-                  N_zoom=25,
-                  N_truncate=N_Truncate)
+    h2co = pc.get_data(path_h2co, n)
+    h2co = h2co[ind_r:]
+    h2co, _ = pc.adjust_data_and_reshape(h2co, ppifg)
+    h2co = h2co[:5]
 
-# %% the whole thing
-# N_shocks = 10
-# N_Truncate = 20
-# CO = np.zeros((N_shocks, ppifg * N_Truncate))
-# H2CO = np.zeros((N_shocks, ppifg * N_Truncate))
+    h2co = pc.Phase_Correct(h2co, ppifg, 25, False)
+    H2CO[n] = np.mean(h2co, 0)
+    print(n)
+
+# %%
+# FFT_H2CO = pc.fft(H2CO, 1)
+# FFT_H2CO_CORR = np.copy(FFT_H2CO)
+# ll, ul = 1120 + ppifg // 2, 3600 + ppifg // 2
 #
-# for i in range(1, N_shocks + 1):
-#     co, ind = analyze(path_co, ppifg, i, plot=False, IND_TOTAL_TO_THROW=None, N_zoom=50, N_truncate=N_Truncate)
-#     h2co, _ = analyze(path_h2co, ppifg, i, plot=False, IND_TOTAL_TO_THROW=ind, N_zoom=25, N_truncate=N_Truncate)
+# ref = FFT_H2CO_CORR[0]
+# freq = np.fft.fftfreq(len(ref))
+# for n, i in enumerate(FFT_H2CO_CORR):
+#     phase1 = np.unwrap(np.arctan2(ref[ll:ul].imag, ref[ll:ul].real))
+#     phase2 = np.unwrap(np.arctan2(i[ll:ul].imag, i[ll:ul].real))
 #
-#     CO[i - 1] = co.flatten()
-#     H2CO[i - 1] = h2co.flatten()
+#     pfit1 = np.polyfit(np.arange(len(phase1)), phase1, 1)
+#     pfit2 = np.polyfit(np.arange(len(phase2)), phase2, 1)
+#     p1 = np.poly1d(pfit1)
+#     p2 = np.poly1d(pfit2)
 #
-#     print(i)
+#     diff = pfit1 - pfit2
+#     phase_diff = (freq * diff[0] + diff[1]) * 2 * np.pi
+#     i = np.fft.ifftshift(i)
+#     i *= np.exp(1j * phase_diff)
+#     i = np.fft.fftshift(i)
+#     FFT_H2CO_CORR[n] = i
+#
+#     print(n)
 #
 # # %%
-# # center = ppifg // 2
-# # zoom = H2CO[:, center - 200:center + 201]
-# # zoom = (zoom.T - np.mean(zoom, 1)).T
-# # window = np.blackman(25)
-# # left = (len(zoom[0]) - len(window)) // 2
-# # right = len(zoom[0]) - len(window) - left
-# # window = np.pad(window, (left, right), constant_values=0)
-# # zoom *= window
-# #
-# # ftzoom = fft(zoom, 1)
-# # ftzoom *= np.conj(ftzoom[0])
-# # corr = np.pad(ftzoom, ())
+# avg_fft = np.mean(FFT_H2CO_CORR, 0)
+# plt.plot(avg_fft.__abs__())
+
+# %%
+fft = pc.fft(H2CO, 1)
+phase = np.arctan2(fft.imag, fft.real)
+fft *= np.exp(-1j * phase)
+back = pc.ifft(fft, 1)
+mean = np.mean(back, 0)
+plt.plot(pc.fft(mean).__abs__())
+
+# %%
+# fft1 = pc.fft(H2CO[0])
+# fft2 = pc.fft(H2CO[-1])
+#
+# # %%
+# ll, ul = 1120 + ppifg // 2, 3600 + ppifg // 2
+# fft1 = fft1[ll:ul]
+# fft2 = fft2[ll:ul]
+#
+# # %%
+# phase1 = np.unwrap(np.arctan2(fft1.imag, fft1.real))
+# phase2 = np.unwrap(np.arctan2(fft2.imag, fft2.real))
+#
+# # %%
+# plt.plot(rad_to_deg(phase1))
+# plt.plot(rad_to_deg(phase2))
+#
+# # %%
+# pfit1 = np.polyfit(np.arange(len(phase1)), phase1, 1)
+# pfit2 = np.polyfit(np.arange(len(phase1)), phase2, 1)
+# p1 = np.poly1d(pfit1)
+# p2 = np.poly1d(pfit2)
+#
+# # %%
+# plt.plot(rad_to_deg(p1(np.arange(len(phase1)))))
+# plt.plot(rad_to_deg(p2(np.arange(len(phase1)))))
+#
+# # %%
+# fft1 = pc.fft(H2CO[0])
+# fft2 = pc.fft(H2CO[-1])
+# diff = pfit1 - pfit2
+# freq = np.fft.fftfreq(len(fft1))
+# phase_diff = (freq * diff[0] + diff[1]) * 2 * np.pi
+# fft2 = np.fft.ifftshift(fft2)
+# fft2 *= np.exp(1j * phase_diff)
+# fft2 = np.fft.fftshift(fft2)
+#
+# # %%
+# print(np.mean(abs(fft1 - fft2) ** 2))
+#
+# # %%
+# plt.figure()
+# plt.plot(fft1.real)
+# plt.plot(fft2.real)
+# plt.plot(fft1.imag)
+# plt.plot(fft2.imag)
+# plt.xlim(ppifg // 2, 14000)
+# plt.ylim(-4000, 8000)
