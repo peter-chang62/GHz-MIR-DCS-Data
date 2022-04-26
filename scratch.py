b8710e6e90bd6968edc533b7cde46def5e70e1d7
Peter <pech6593@colorado.edu>
2022-04-26 17:49:30 -0600

    latest update

--- scratch.py
+++ scratch.py
@@ -26,48 +26,56 @@ for n in range(248):
     h2co = pc.get_data(path_h2co, n)
     h2co = h2co[ind_r:]
     h2co, _ = pc.adjust_data_and_reshape(h2co, ppifg)
-    h2co = h2co[:5]
+    h2co = h2co[:100]
 
     h2co = pc.Phase_Correct(h2co, ppifg, 25, False)
     H2CO[n] = np.mean(h2co, 0)
     print(n)
 
 # %%
-# FFT_H2CO = pc.fft(H2CO, 1)
-# FFT_H2CO_CORR = np.copy(FFT_H2CO)
-# ll, ul = 1120 + ppifg // 2, 3600 + ppifg // 2
-#
-# ref = FFT_H2CO_CORR[0]
-# freq = np.fft.fftfreq(len(ref))
-# for n, i in enumerate(FFT_H2CO_CORR):
-#     phase1 = np.unwrap(np.arctan2(ref[ll:ul].imag, ref[ll:ul].real))
-#     phase2 = np.unwrap(np.arctan2(i[ll:ul].imag, i[ll:ul].real))
-#
-#     pfit1 = np.polyfit(np.arange(len(phase1)), phase1, 1)
-#     pfit2 = np.polyfit(np.arange(len(phase2)), phase2, 1)
-#     p1 = np.poly1d(pfit1)
-#     p2 = np.poly1d(pfit2)
-#
-#     diff = pfit1 - pfit2
-#     phase_diff = (freq * diff[0] + diff[1]) * 2 * np.pi
-#     i = np.fft.ifftshift(i)
-#     i *= np.exp(1j * phase_diff)
-#     i = np.fft.fftshift(i)
-#     FFT_H2CO_CORR[n] = i
-#
-#     print(n)
-#
-# # %%
-# avg_fft = np.mean(FFT_H2CO_CORR, 0)
-# plt.plot(avg_fft.__abs__())
+FFT_H2CO = pc.fft(H2CO, 1)
+DIFF = np.zeros(len(FFT_H2CO))
+ll, ul = 1120 + ppifg // 2, 3600 + ppifg // 2
+
+ref = FFT_H2CO[0]
+freq = np.fft.fftfreq(len(ref))
+for n, i in enumerate(FFT_H2CO):
+    phase1 = np.unwrap(np.arctan2(ref[ll:ul].imag, ref[ll:ul].real))
+    phase2 = np.unwrap(np.arctan2(i[ll:ul].imag, i[ll:ul].real))
+
+    pfit1 = np.polyfit(np.arange(len(phase1)), phase1, 1)
+    pfit2 = np.polyfit(np.arange(len(phase2)), phase2, 1)
+    p1 = np.poly1d(pfit1)
+    p2 = np.poly1d(pfit2)
+
+    diff = pfit1 - pfit2
+
+    remainder = diff % (2 * np.pi)
+    diff = np.where(remainder < np.pi, 0, np.pi)
+
+    phase_diff = diff[1]  # higher order first
+    i *= np.exp(1j * phase_diff)
+    FFT_H2CO[n] = i
+    DIFF[n] = diff[1]  # higher order first
+
+    print(n)
 
 # %%
-fft = pc.fft(H2CO, 1)
-phase = np.arctan2(fft.imag, fft.real)
-fft *= np.exp(-1j * phase)
-back = pc.ifft(fft, 1)
-mean = np.mean(back, 0)
-plt.plot(pc.fft(mean).__abs__())
+back = pc.ifft(FFT_H2CO, 1)
+
+# %%
+back = back.real
+back = (back.T - np.mean(back, 1)).T
+# back = (back.T / np.max(abs(back), 1)).T
+
+# %%
+back = pc.Phase_Correct(back, ppifg, 25, True)
+
+# %%
+plt.figure()
+avg = np.mean(back, 0)
+fft = pc.fft(avg)
+plt.plot(fft.__abs__())
 
 # %%
 # fft1 = pc.fft(H2CO[0])
@@ -100,14 +108,8 @@ plt.plot(pc.fft(mean).__abs__())
 # fft1 = pc.fft(H2CO[0])
 # fft2 = pc.fft(H2CO[-1])
 # diff = pfit1 - pfit2
-# freq = np.fft.fftfreq(len(fft1))
-# phase_diff = (freq * diff[0] + diff[1]) * 2 * np.pi
-# fft2 = np.fft.ifftshift(fft2)
+# phase_diff = diff[1]
 # fft2 *= np.exp(1j * phase_diff)
-# fft2 = np.fft.fftshift(fft2)
-#
-# # %%
-# print(np.mean(abs(fft1 - fft2) ** 2))
 #
 # # %%
 # plt.figure()
@@ -117,3 +119,9 @@ plt.plot(pc.fft(mean).__abs__())
 # plt.plot(fft2.imag)
 # plt.xlim(ppifg // 2, 14000)
 # plt.ylim(-4000, 8000)
+#
+# # %%
+# back = pc.ifft(fft2)
+# plt.plot(H2CO[0])
+# plt.plot(H2CO[-1])
+# plt.plot(back.real)
