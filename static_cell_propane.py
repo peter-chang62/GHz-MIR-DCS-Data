b9219a33b8707ee029a7b07e9a362968ea9afbbd
Peter <pech6593@colorado.edu>
2022-06-24 10:53:58 -0600

    update

--- static_cell_propane.py
+++ static_cell_propane.py
@@ -8,7 +8,7 @@ import digital_phase_correction as dpc
 
 clipboard_and_style_sheet.style_sheet()
 
-# %%
+# %% ___________________________________________________________________________________________________________________
 """Vacuum Background """
 # ppifg = 198850
 # center = ppifg // 2
@@ -20,19 +20,19 @@ clipboard_and_style_sheet.style_sheet()
 # data = np.fromfile(r'D:\ShockTubeData\static cell/with_cell_vacuum_bckgnd_10030x199346.bin', '<h')
 # data, _ = pc.adjust_data_and_reshape(data, ppifg)
 
-# %%
+# %% ___________________________________________________________________________________________________________________
 """Mixture Data """
-# ppifg = 198850
-# center = ppifg // 2
-# data = np.fromfile(r'D:\ShockTubeData\static cell/cell_with_mixture_and_4_5_filter_at200_4998x198850.bin', '<h')
-# data, _ = pc.adjust_data_and_reshape(data, ppifg)
-
 ppifg = 198850
 center = ppifg // 2
-data = np.fromfile(r'D:\ShockTubeData\static cell/cell_with_mixture_10030x198850.bin', '<h')
+data = np.fromfile(r'D:\ShockTubeData\static cell/cell_with_mixture_and_4_5_filter_at200_4998x198850.bin', '<h')
 data, _ = pc.adjust_data_and_reshape(data, ppifg)
 
-# %%
+# ppifg = 198850
+# center = ppifg // 2
+# data = np.fromfile(r'D:\ShockTubeData\static cell/cell_with_mixture_10030x198850.bin', '<h')
+# data, _ = pc.adjust_data_and_reshape(data, ppifg)
+
+# %% ___________________________________________________________________________________________________________________
 # ll_freq, ul_freq = 0.425, 0.452  # 3.5 um
 # ll_freq, ul_freq = 0.325, 0.345  # 4.5 um
 ll_freq, ul_freq = 0.325, 0.487  # no filter
@@ -49,23 +49,23 @@ while h < len(data):
 avg = np.mean(data, 0)
 fft = pc.fft(avg)
 
-# %%
+# %% ___________________________________________________________________________________________________________________
 fr = 1010e6 - 10008148.6
 freq = np.arange(0, center) * fr
 wl = sc.c * 1e6 / freq
 
-# %%
+# %% ___________________________________________________________________________________________________________________
 # ll, ul = np.argmin(abs(wl - 3.6)), np.argmin(abs(wl - 3.3))
 # ll, ul = np.argmin(abs(wl - 4.8)), np.argmin(abs(wl - 4.3))
 ll, ul = np.argmin(abs(wl - 5.0)), np.argmin(abs(wl - 3.0))
 
-# %%
+# %% ___________________________________________________________________________________________________________________
 fig = plt.figure()
 plt.plot(wl[ll:ul], fft[center:][ll:ul].__abs__())
 plt.xlabel("wavelength ($\mathrm{\mu m}$)")
 plt.ylabel("a.u.")
 
-# %%
+# # %% ___________________________________________________________________________________________________________________
 """Looking at phase corrected data """
 
 
@@ -78,18 +78,18 @@ def get_freq_wl_ll_ul(ppifg):
     return freq, wl, ll, ul
 
 
-# %%
+# %% ___________________________________________________________________________________________________________________
 data_ = np.fromfile(r'D:\ShockTubedata\static cell\PHASE_CORRECTED/cell_with_mixture_averaged.bin')
 bckgnd = np.fromfile(r'D:\ShockTubedata\static cell\PHASE_CORRECTED/with_cell_vacuum_bckgnd_averaged.bin')
 
 freq1, wl1, ll1, ul1 = get_freq_wl_ll_ul(len(data_))
 freq2, wl2, ll2, ul2 = get_freq_wl_ll_ul(len(bckgnd))
 
-# %%
+# %% ___________________________________________________________________________________________________________________
 fft_data_ = pc.fft(data_)[len(data_) // 2:].__abs__()
 fft_bckgnd = pc.fft(bckgnd)[len(bckgnd) // 2:].__abs__()
 
-# %%
+# %% ___________________________________________________________________________________________________________________
 fig = plt.figure()
 plt.plot(wl1[ll1:ul1], fft_data_[ll1:ul1], label='data_')
 plt.plot(wl2[ll2:ul2], fft_bckgnd[ll2:ul2], label='background')
@@ -97,7 +97,7 @@ plt.legend(loc='best')
 plt.xlabel("wavelength ($\mathrm{\mu m}$)")
 plt.ylabel("a.u.")
 
-# %%
+# %% ___________________________________________________________________________________________________________________
 fig = plt.figure()
 plt.plot(wl[ll:ul], -np.log(fft_data_[ll:ul] / fft_bckgnd[ll:ul]))
 plt.xlabel("wavelength ($\mathrm{\mu m}$)")
